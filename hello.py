bb6f83e6792d45f363e309c171a4268a6d1593ee
Viperzk <zekaichrn@gmail.com>
2026-09-04 16:36:57 +0800

    Third Post

--- hello.py
+++ hello.py
@@ -10,3 +10,12 @@ followers = 100
 print("Username:", username)
 print("Bio:", bio)
 print("Followers:", followers)
+
+followers += 50
+print("day 1:", followers)
+
+followers += 20
+print("day 2:", followers)
+
+followers += 50
+print("day 3:", followers)
